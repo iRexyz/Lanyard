4f75967442950224c3334c95e799206477ba4efa
iRexyz <81679033+iRexyz@users.noreply.github.com>
2022-09-18 12:14:28 +0300

    Update LanyardTypes.ts

--- src/LanyardTypes.ts
+++ src/LanyardTypes.ts
@@ -6,28 +6,15 @@ export interface Root {
 }
 
 export interface Data {
-    spotify: Spotify;
-    listening_to_spotify: boolean;
+
     discord_user: DiscordUser;
-    discord_status: string;
-    activities: Activity[];
     active_on_discord_mobile: boolean;
     active_on_discord_desktop: boolean;
 }
 
-export interface Spotify {
-    track_id: string;
-    timestamps: Timestamps;
-    song: string;
-    artist: string;
-    album_art_url: string;
-    album: string;
-}
 
-export interface Timestamps {
-    start: number;
-    end: number;
-}
+
+
 
 export interface DiscordUser {
     username: string;
@@ -37,23 +24,7 @@ export interface DiscordUser {
     avatar: string;
 }
 
-export interface Activity {
-    type: number;
-    state: string;
-    name: string;
-    id: string;
-    emoji?: Emoji;
-    created_at: number;
-    application_id?: string;
-    timestamps?: Timestamps2;
-    sync_id?: string;
-    session_id?: string;
-    party?: Party;
-    flags?: number;
-    details?: string;
-    assets?: Assets;
-    buttons?: string[];
-}
+
 
 export interface Emoji {
     name: string;
